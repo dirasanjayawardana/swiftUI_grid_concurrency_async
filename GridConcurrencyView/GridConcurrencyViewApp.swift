9190447433d3f74546949c822690ff581b42df22
dira sanjaya wardana <dirawardana.dw@gmail.com>
2024-04-22 08:45:18 +0700

    Initial Commit

---
+++ GridConcurrencyView/GridConcurrencyViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridConcurrencyViewApp.swift
+//  GridConcurrencyView
+//
+//  Created by MACBOOK PRO on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct GridConcurrencyViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
